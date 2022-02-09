548f0f691be78e6d6719d279efeec3ae7f716bff
Rishabh B <59335537+rbSparky@users.noreply.github.com>
2022-02-09 22:02:37 +0530

    Add files via upload

---
+++ landing/src/pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/Head'
+
+const about = () => {
+	return (
+		<div>
+		<Head> 
+		<title>About</title>
+		</Head>
+		<h1>About</h1>
+		</div>
+	)
+}
+export default about
---
+++ landing/src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'COLINFO',
+  title: 'Colinfo',
+  description: 'Your college counselling helper',
+  locale: 'en',
+};
